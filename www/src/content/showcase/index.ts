0dfebdada30bbc92c349d242e2c07bf6820efe93
Jaydan Urwin <jaydan@treefarm.studio>
2026-03-14 22:07:00 -0600

    fix deploy

---
+++ www/src/content/showcase/index.ts
@@ -0,0 +1,87 @@
+export interface ShowcaseSite {
+  title: string;
+  url: string;
+  image: string;
+  tags: string[];
+}
+
+export const showcaseSites: ShowcaseSite[] = [
+  {
+    title: "Sapling",
+    url: "https://sapling-is.land",
+    image: "/assets/images/showcase/sapling.png",
+    tags: ["Open Source"],
+  },
+  {
+    title: "Treefarm",
+    url: "https://treefarm.studio",
+    image: "/assets/images/showcase/treefarm.png",
+    tags: ["Agency"],
+  },
+  {
+    title: "Spokesite",
+    url: "https://spokesite.com",
+    image: "/assets/images/showcase/spokesite.png",
+    tags: ["SaaS"],
+  },
+  {
+    title: "Jaydan Urwin",
+    url: "https://jaydanurw.in",
+    image: "/assets/images/showcase/jaydan.png",
+    tags: ["Portfolio"],
+  },
+  {
+    title: "SnipClip",
+    url: "https://snipclip.com",
+    image: "/assets/images/showcase/snipclip.png",
+    tags: ["SaaS"],
+  },
+  {
+    title: "Tree0",
+    url: "https://tree0.com",
+    image: "/assets/images/showcase/tree0.png",
+    tags: ["Product"],
+  },
+  {
+    title: "Plately",
+    url: "https://plately.com",
+    image: "/assets/images/showcase/plately.png",
+    tags: ["Product"],
+  },
+  {
+    title: "Cardoza",
+    url: "https://cardoza.com",
+    image: "/assets/images/showcase/cardoza.png",
+    tags: ["Business"],
+  },
+  {
+    title: "Iate",
+    url: "https://iate.com",
+    image: "/assets/images/showcase/iate.png",
+    tags: ["SaaS"],
+  },
+  {
+    title: "Leana",
+    url: "https://leana.com",
+    image: "/assets/images/showcase/leana.png",
+    tags: ["Business"],
+  },
+  {
+    title: "Hockema",
+    url: "https://hockema.com",
+    image: "/assets/images/showcase/hockema.png",
+    tags: ["Portfolio"],
+  },
+  {
+    title: "McFates",
+    url: "https://mcfates.com",
+    image: "/assets/images/showcase/mcfates.png",
+    tags: ["Business"],
+  },
+  {
+    title: "Peterson",
+    url: "https://peterson.com",
+    image: "/assets/images/showcase/peterson.png",
+    tags: ["Business"],
+  },
+];
